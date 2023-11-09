9fe3f62f736cf3ede5bc5612ef4ac4048f67ef03
Angel Motta <angel.motta@utec.edu.pe>
2023-11-08 22:27:14 -0500

    feat: implementation done

--- src/App.js
+++ src/App.js
@@ -1,77 +1,134 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Formik } from "formik";
+import { Formik, Form, useField } from "formik";
 import * as Yup from "yup";
 // import "./styles.css";
 
+const MyTextInput = ({ label, ...props }) => {
+    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+    // which we can spread on <input>. We can use field meta to show an error
+    // message if the field is invalid and it has been touched (i.e. visited)
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input className="text-input" {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    );
+};
+
+const MyCheckbox = ({ children, ...props }) => {
+    // React treats radios and checkbox inputs differently from other input types: select and textarea.
+    // Formik does this too! When you specify `type` to useField(), it will
+    // return the correct bag of props for you -- a `checked` prop will be included
+    // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+    const [field, meta] = useField({ ...props, type: "checkbox" });
+    return (
+        <div>
+            <label className="checkbox-input">
+                <input type="checkbox" {...field} {...props} />
+                {children}
+            </label>
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </div>
+    );
+};
+
+const MySelect = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <div>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <select {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </div>
+    );
+};
+
 const SignupForm = () => {
     return (
-        <Formik
-            initialValues={{
-                firstName: "",
-                lastName: "",
-                email: "",
-            }}
-            validationSchema={Yup.object({
-                firstName: Yup.string()
-                    .max(20, "Must be 20 characters or less")
-                    .required("Required"),
-                lastName: Yup.string()
-                    .max(20, "Must be 20 characters or less")
-                    .required("Required"),
-                email: Yup.string()
-                    .email("Invalid email address")
-                    .required("Required"),
-            })}
-            onSubmit={(values) => {
-                alert(JSON.stringify(values, null, 2));
-            }}
-        >
-            {(formik) => (
-                <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="firstName">First Name</label>
-                    <input
-                        id="firstName"
+        <>
+            <h1>Subscribe!</h1>
+            <Formik
+                initialValues={{
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    acceptedTerms: false, // checkbox
+                    jobType: "", // select
+                }}
+                validationSchema={Yup.object({
+                    firstName: Yup.string()
+                        .max(20, "Must be 20 characters or less")
+                        .required("Required"),
+                    lastName: Yup.string()
+                        .max(20, "Must be 20 characters or less")
+                        .required("Required"),
+                    email: Yup.string()
+                        .email("Invalid email address")
+                        .required("Required"),
+                    acceptedTerms: Yup.boolean()
+                        .required("Required")
+                        .oneOf(
+                            [true],
+                            "You must accept the terms and conditions."
+                        ),
+                    jobType: Yup.string()
+                        .oneOf(
+                            ["designer", "development", "product", "other"],
+                            "Invalid Job Type"
+                        )
+                        .required("Required"),
+                })}
+                onSubmit={(values) => {
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                <Form>
+                    <MyTextInput
+                        label="First Name"
                         name="firstName"
                         type="text"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.firstName}
+                        placeholder="Jane"
                     />
-                    {formik.touched.firstName && formik.errors.firstName ? (
-                        <div className="error">{formik.errors.firstName}</div>
-                    ) : null}
 
-                    <label htmlFor="lastName">Last Name</label>
-                    <input
-                        id="lastName"
+                    <MyTextInput
+                        label="Last Name"
                         name="lastName"
                         type="text"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.lastName}
+                        placeholder="Doe"
                     />
-                    {formik.touched.lastName && formik.errors.lastName ? (
-                        <div className="error">{formik.errors.lastName}</div>
-                    ) : null}
 
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                        id="email"
+                    <MyTextInput
+                        label="Email Address"
                         name="email"
                         type="email"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.email}
+                        placeholder="jane@formik.com"
                     />
-                    {formik.touched.email && formik.errors.email ? (
-                        <div className="error">{formik.errors.email}</div>
-                    ) : null}
+
+                    <MySelect label="Job Type" name="jobType">
+                        <option value="">Select a job type</option>
+                        <option value="designer">Designer</option>
+                        <option value="development">Developer</option>
+                        <option value="product">Product Manager</option>
+                        <option value="other">Other</option>
+                    </MySelect>
+
+                    <MyCheckbox name="acceptedTerms">
+                        I accept the terms and conditions
+                    </MyCheckbox>
 
                     <button type="submit">Submit</button>
-                </form>
-            )}
-        </Formik>
+                </Form>
+            </Formik>
+        </>
     );
 };
 
